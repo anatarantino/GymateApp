671f2418ca1dfadfc4eda8b964308112c84d8e4e
anatarantino <58373829+anatarantino@users.noreply.github.com>
2021-05-24 20:44:12 -0300

    avances :)

---
+++ app/src/main/java/ar/edu/itba/example/gymateapp/view/activities/LoadingActivity.java
@@ -0,0 +1,16 @@
+package ar.edu.itba.example.gymateapp.view.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ar.edu.itba.example.gymateapp.R;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
---
+++ app/src/main/java/ar/edu/itba/example/gymateapp/view/fragments/RoutineExecutionFragment.java
@@ -0,0 +1,14 @@
+package ar.edu.itba.example.gymateapp.view.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class RoutineExecutionFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
