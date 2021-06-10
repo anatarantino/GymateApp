3a16920edad62f3c6363607fff5331f27e4df71c
anatarantino <58373829+anatarantino@users.noreply.github.com>
2021-06-10 18:32:22 -0300

    fijarse si anda spinner :)
    Co-Authored-By: Martina Rodriguez <58373738+martirodriguez98@users.noreply.github.com>

--- app/src/main/java/ar/edu/itba/example/gymateapp/view/fragments/MyRoutinesFragment.java
+++ app/src/main/java/ar/edu/itba/example/gymateapp/view/fragments/MyRoutinesFragment.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -44,8 +46,16 @@ public class MyRoutinesFragment extends Fragment implements RoutinesAdapter.Item
         seedRoutines();
         RoutinesAdapter adapter = new RoutinesAdapter(myRoutineList,this);
         recyclerRoutine.setAdapter(adapter);
-        Button sortBtn = view.findViewById(R.id.button5);
-        sortBtn.setOnClickListener(v -> Navigation.findNavController(view).navigate(MyRoutinesFragmentDirections.actionNavigationMyRoutinesToSortByFragment()));
+//        Button sortBtn = view.findViewById(R.id.button5);
+//        sortBtn.setOnClickListener(v -> Navigation.findNavController(view).navigate(MyRoutinesFragmentDirections.actionNavigationMyRoutinesToSortByFragment()));
+        Spinner spinner = (Spinner) view.findViewById(R.id.sortby_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> spinner_adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.spinner_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        spinner_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(spinner_adapter);
         ((MainActivity) getActivity()).setNavigationVisibility(true);
         return view;
     }
