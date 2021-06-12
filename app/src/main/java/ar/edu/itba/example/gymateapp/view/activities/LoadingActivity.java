f4061e012d9d525d0b9cedb43e98e2fb27bf4742
martina <martinrodriguez@itba.edu.ar>
2021-06-12 02:22:12 -0300

    prueba para cambiar el login
    agregamos el loginFragment y ya no esta todo en la activity
    
    Co-Authored-By: Ana Tarantino <atarantino@itba.edu.ar>

--- app/src/main/java/ar/edu/itba/example/gymateapp/view/activities/LoadingActivity.java
+++ app/src/main/java/ar/edu/itba/example/gymateapp/view/activities/LoadingActivity.java
@@ -1,25 +1,67 @@
 package ar.edu.itba.example.gymateapp.view.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavDeepLinkBuilder;
 
 import java.util.concurrent.Executor;
 
 import ar.edu.itba.example.gymateapp.R;
+import ar.edu.itba.example.gymateapp.model.AppPreferences;
 
 public class LoadingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        AppPreferences preferences = new AppPreferences(this.getApplication());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+//        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+//            Intent intent = new Intent(this, LoginActivity.class);
+//            startActivity(intent);
+//        },2000);
+        new Handler().postDelayed(()-> {
+            Intent appLinkIntent = getIntent();
+            Uri appLinkData = appLinkIntent.getData();
+            if(appLinkData != null) { //inicio desde un link
+                String routineId = appLinkData.getLastPathSegment();
+                newActivity(preferences,routineId);
+            }else{
+                Intent intent;
+                if(preferences.getAuthToken() != null){
+                    intent = new Intent(LoadingActivity.this, MainActivity.class);
+                }else {
+                    intent = new Intent(LoadingActivity.this, LoginActivity.class);
+                }
+                startActivity(intent);
+            }
         },2000);
+    }
+
+    private void newActivity(AppPreferences preferences, String routineId){
+        Bundle bundle = new Bundle();
+        bundle.putInt("routineId",Integer.parseInt(routineId));
+        if(preferences.getAuthToken() != null){
+            new NavDeepLinkBuilder(this).setComponentName(MainActivity.class)
+                    .setGraph(R.navigation.mobile_navigation).setDestination(R.id.routine_detail)
+                    .setArguments(bundle)
+                    .createTaskStackBuilder()
+                    .startActivities();
+            return;
+        }
+        Intent auxIntent = new Intent(LoadingActivity.this, LoginActivity.class);
+        putAndStart(routineId, auxIntent);
+    }
+
+    private void putAndStart(String routineId, Intent intent){
+        intent.putExtra("RoutineId",routineId);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
     }
 }
--- app/src/main/java/ar/edu/itba/example/gymateapp/view/fragments/LoginFragment.java
+++ app/src/main/java/ar/edu/itba/example/gymateapp/view/fragments/LoginFragment.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavDeepLinkBuilder;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -22,6 +23,7 @@ import org.jetbrains.annotations.NotNull;
 
 import ar.edu.itba.example.gymateapp.R;
 import ar.edu.itba.example.gymateapp.databinding.ActivityLoginBinding;
+import ar.edu.itba.example.gymateapp.databinding.FragmentLoginBinding;
 import ar.edu.itba.example.gymateapp.view.activities.MainActivity;
 import ar.edu.itba.example.gymateapp.viewModel.UserViewModel;
 
@@ -33,7 +35,7 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        FragmentLoginBinding binding = FragmentLoginBinding.inflate(getLayoutInflater());
         username = binding.username;
         password = binding.password;
 
@@ -47,6 +49,7 @@ public class LoginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        viewModel = new ViewModelProvider(getActivity()).get(UserViewModel.class);
     }
 
     private void tryLogin() {
