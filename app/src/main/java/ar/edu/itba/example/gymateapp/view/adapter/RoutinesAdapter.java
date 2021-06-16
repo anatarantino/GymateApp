c0dd6de77271cdb2f633d408cc3722012898c077
martina <martinrodriguez@itba.edu.ar>
2021-06-16 02:07:01 -0300

    ratingCard y categoria
    Co-Authored-By: Ana Tarantino <atarantino@itba.edu.ar>

--- app/src/main/java/ar/edu/itba/example/gymateapp/view/adapter/RoutinesAdapter.java
+++ app/src/main/java/ar/edu/itba/example/gymateapp/view/adapter/RoutinesAdapter.java
@@ -13,6 +13,9 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.chip.Chip;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -23,80 +26,6 @@ import ar.edu.itba.example.gymateapp.databinding.RoutineCardBinding;
 import ar.edu.itba.example.gymateapp.model.RoutineCredentials;
 import ar.edu.itba.example.gymateapp.view.classes.RoutineData;
 import ar.edu.itba.example.gymateapp.view.fragments.ClickOnRoutine;
-
-//public class RoutinesAdapter extends RecyclerView.Adapter<RoutinesAdapter.RoutineViewHolder> {
-//
-//    ArrayList<RoutineData> routinesList;
-//    private ItemClickListener listener;
-//
-//    public RoutinesAdapter(ArrayList<RoutineData> routinesList, ItemClickListener listener) {
-//        this.routinesList = routinesList;
-//        this.listener = listener;
-//    }
-//
-//    @Override
-//    public RoutineViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.routine_card,null,false);
-//        return new RoutineViewHolder(view);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull @NotNull RoutinesAdapter.RoutineViewHolder holder, int position) {
-//        RoutineData routine = routinesList.get(position);
-//        int id = routine.cat.id;
-//        switch(id) {
-//            //aca vamos a tener que elegir una img para cada cat
-//            case 1:
-//                holder.img.setImageResource(R.drawable.fit);
-//                routine.setImg(String.valueOf(R.drawable.fit));
-//                break;
-//            case 2:
-//                holder.img.setImageResource(R.drawable.fit2);
-//                routine.setImg(String.valueOf(R.drawable.fit2));
-//                break;
-//        }
-//        holder.id.setText(String.valueOf(routinesList.get(position).id));
-//        holder.txtTitle.setText(routinesList.get(position).title);
-//        holder.txtCreator.setText(routinesList.get(position).creator);
-//        holder.rating.setRating(routinesList.get(position).rating);
-//
-//        //listener de la rutina clickeada
-//
-//        holder.itemView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                listener.onItemClick(routinesList.get(position));
-//            }
-//        });
-//
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return routinesList.size();
-//    }
-//
-//    public class RoutineViewHolder extends RecyclerView.ViewHolder {
-//        TextView id,txtTitle,txtCreator;
-//        ImageView img;
-//        RatingBar rating;
-//
-//        public RoutineViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//            id = (TextView) itemView.findViewById(R.id.routineId);
-//            txtTitle = (TextView) itemView.findViewById(R.id.title);
-//            txtCreator = (TextView) itemView.findViewById(R.id.creator);
-//            img = (ImageView) itemView.findViewById(R.id.image);
-//            rating = (RatingBar) itemView.findViewById(R.id.routineRatingBar);
-//
-//        }
-//    }
-//
-//    public interface ItemClickListener{
-//        public void onItemClick(RoutineData routineData);
-//    }
-//
-//}
 
 public class RoutinesAdapter extends RecyclerView.Adapter<RoutinesAdapter.RoutineViewHolder> {
 
@@ -143,6 +72,7 @@ public class RoutinesAdapter extends RecyclerView.Adapter<RoutinesAdapter.Routin
         holder.txtTitle.setText(routinesList.get(position).getName());
         holder.txtCreator.setText(routinesList.get(position).getUser().getUsername());
         holder.rating.setRating(routinesList.get(position).getAverageRating());
+        holder.category.setText(routinesList.get(position).getCategory().getName());
 
         //listener de la rutina clickeada
 
@@ -175,6 +105,7 @@ public class RoutinesAdapter extends RecyclerView.Adapter<RoutinesAdapter.Routin
         TextView id,txtTitle,txtCreator;
         ImageView img;
         RatingBar rating;
+        Chip category;
 
         public RoutineViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -183,6 +114,7 @@ public class RoutinesAdapter extends RecyclerView.Adapter<RoutinesAdapter.Routin
             txtCreator = (TextView) itemView.findViewById(R.id.creator);
             img = (ImageView) itemView.findViewById(R.id.image);
             rating = (RatingBar) itemView.findViewById(R.id.routineRatingBar);
+            category = (Chip) itemView.findViewById(R.id.categoryChip);
         }
     }
 
