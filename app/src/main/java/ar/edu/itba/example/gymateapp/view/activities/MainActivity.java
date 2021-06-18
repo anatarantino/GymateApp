151b0dab8ae07ae9ce6132172e652fd9d57131b3
martina <martinrodriguez@itba.edu.ar>
2021-06-17 21:20:58 -0300

    .
    Co-Authored-By: Ana Tarantino <atarantino@itba.edu.ar>

--- app/src/main/java/ar/edu/itba/example/gymateapp/view/activities/MainActivity.java
+++ app/src/main/java/ar/edu/itba/example/gymateapp/view/activities/MainActivity.java
@@ -50,9 +50,10 @@ public class MainActivity extends AppCompatActivity {
             Log.e("Id del uri: ",uri.getLastPathSegment());
             Bundle bundle = new Bundle();
             bundle.putString("RoutineId",uri.getLastPathSegment());
+
             //bundle.putString("RoutineId",uri.getLastPathSegment());
 //            bundle.putInt("RoutineId",Integer.parseInt(uri.getLastPathSegment()));
-            Log.e("mainAct","entre!! con el id: " + bundle.getBundle("RoutineId"));
+            Log.e("mainAct","entre!! con el id: " + bundle.getString("RoutineId"));
             navController.navigate(R.id.action_navigation_home_to_routineDetailFragment,bundle);
         }
 
